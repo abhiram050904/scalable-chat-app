0b51bd3fcee5d71101921758b3cff021c9e82702
ABHIRAM GATTAMNENI <abhiram050904@gmail.com>
2025-04-15 13:47:48 +0530

    chat-feature-updated

--- frontend/components/chat/ChatBottomBar.tsx
+++ frontend/components/chat/ChatBottomBar.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { Image } from 'lucide-react'
+import React, { useState } from 'react'
+import { Textarea } from '../ui/textarea'
+import EmojiPicker from './EmojiPicker'
 
 const ChatBottomBar = () => {
+  const [message,setMessage]=useState("")
   return (
-    <div>
-      
+    <div className='p-2 flex justify-between w-full items-center gap-2'>
+      {!message.trim() && <Image size={20} className='cursor-pointer text-muted-foreground'/>}
+
+      <AnimatePresence>
+        <motion.div
+        layout
+        initial={{opacity:0,scale:1}}
+        animate={{opacity:1,scale:1}}
+        exit={{opacity:0,scale:1}}
+        transition={{
+          opacity:{duration:0.5},
+          layout:{
+            type:"spring",
+            bounce:0.15
+          }
+        }}
+        className='w-full relative'
+        >
+          <Textarea autoComplete='off' placeholder='enter your message here' rows={1} className='w-full border rounded-full flex items-center h-9 resize-none overflow-hidden bg-background min-h-0'
+           value={message} onChange={(e)=>setMessage(e.target.value)}
+          />
+          <div className='absolute right-2 bottom-0.5'>
+            <EmojiPicker onChange={(emoji)=>{
+              setMessage(message+emoji)
+            }}/>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
